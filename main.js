8ef9569f097a7284f85833df5ef12e256cdd35de
rolmaf <61619288+rolmaf@users.noreply.github.com>
2020-04-19 19:31:48 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,100 @@
+let char = "X";
+let turn = 1;
+let area = [
+    ["1" , "2" , "3"] , 
+    ["4" , "5" , "6"] , 
+    ["7" , "8" , "9"] ,
+];
+let turn_see;
+document.querySelector("table").onclick=function(){
+    let col = event.target.cellIndex;
+    let row = event.target.parentNode.rowIndex;
+    turn_see = document.querySelector("p");
+    let cell = event.target;
+    if (area[row][col] != "X" &&  area[row][col] != "0"){
+      if (turn % 2 == 0) {
+          char = "0";
+          area[row][col] = "0";
+          turn_see.innerHTML = "Сейчас ходят Крестики";
+          checkWinner();
+      }
+      else {
+          char = "X";
+          area[row][col] = "X";
+          turn_see.innerHTML = "Сейчас ходят Нолики";
+          checkWinner();
+      }
+      turn++;
+      cell.innerHTML = char;
+    }
+    else {
+      alert("Не жульничайте)");
+    }
+};
+function checkWinner() {
+    if (area[0][0] == "X" && area[0][1] == "X" && area[0][2] == "X") {
+      alert("Крестики Победили!");
+      location.reload();
+    }
+    if (area[1][0] == "X" && area[1][1] == "X" && area[1][2] == "X") {
+      alert("Крестики Победили!");
+      location.reload();
+    }
+    if (area[2][0] == "X" && area[2][1] == "X" && area[2][2] == "X") {
+      alert("Крестики Победили!");
+      location.reload();
+    }
+    if (area[0][0] == "X" && area[1][0] == "X" && area[2][0] == "X") {
+      alert("Крестики Победили!");
+      location.reload();
+    }
+    if (area[0][1] == "X" && area[1][1] == "X" && area[2][1] == "X") {
+      alert("Крестики Победили!");
+      location.reload();
+    }
+    if (area[0][2] == "X" && area[1][2] == "X" && area[2][2] == "X") {
+      alert("Крестики Победили!");
+      location.reload();
+    }
+    if (area[0][0] == "X" && area[1][1] == "X" && area[2][2] == "X") {
+      alert("Крестики Победили!");
+      location.reload();
+    }
+    if (area[0][2] == "X" && area[1][1] == "X" && area[2][0] == "X") {
+      alert("Крестики Победили!");
+      location.reload();
+    }
+  
+    if (area[0][0] == "0" && area[0][1] == "0" && area[0][2] == "0") {
+      alert("Нолики Победили!");
+      location.reload();
+    }
+    if (area[1][0] == "0" && area[1][1] == "0" && area[1][2] == "0") {
+      alert("Нолики Победили!");
+      location.reload();
+    }
+    if (area[2][0] == "0" && area[2][1] == "0" && area[2][2] == "0") {
+      alert("Нолики Победили!");
+      location.reload();
+    }
+    if (area[0][0] == "0" && area[1][0] == "0" && area[2][0] == "0") {
+      alert("Нолики Победили!");
+      location.reload();
+    }
+    if (area[0][1] == "0" && area[1][1] == "0" && area[2][1] == "0") {
+      alert("Нолики Победили!");
+      location.reload();
+    }
+    if (area[0][2] == "0" && area[1][2] == "0" && area[2][2] == "0") {
+      alert("Нолики Победили!");
+      location.reload();
+    }
+    if (area[0][0] == "0" && area[1][1] == "0" && area[2][2] == "0") {
+      alert("Нолики Победили!");
+      location.reload();
+    }
+    if (area[0][2] == "0" && area[1][1] == "0" && area[2][0] == "0") {
+      alert("Нолики Победили!");
+      location.reload();
+    }
+}
